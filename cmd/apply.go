1b3fa523bc43abd6eac6a4fb9aa0177cfa634fd4
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-08-30 21:28:14 +0530

    feat(cmd): use Octyne support in apply command

--- cmd/apply.go
+++ cmd/apply.go
@@ -2,6 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/mythicmc/peridot/utils"
 )
 
 func HandleApplyCommand(args []string) {
@@ -16,5 +21,58 @@ func HandleApplyCommand(args []string) {
 		return
 	}
 
+	affectedServers := make(map[string]struct{})
+	for server := range softwareUpdates {
+		affectedServers[server] = struct{}{}
+	}
+	for server := range serverPropertiesUpdates {
+		affectedServers[server] = struct{}{}
+	}
+	for server := range pluginUpdates {
+		affectedServers[server] = struct{}{}
+	}
+
+	// Send stop signals to all servers
+	fmt.Println("Stopping affected servers via Octyne...")
+	for server := range affectedServers {
+		err := utils.OctyneTerminateServer(server)
+		if err != nil {
+			log.Fatalf("Error stopping server %s: %v\n", server, err)
+		}
+	}
+
+	// Wait for servers to stop
+	var wg sync.WaitGroup
+	for server := range affectedServers {
+		wg.Add(1)
+		go func(s string) {
+			defer wg.Done()
+			for {
+				<-time.After(2 * time.Second)
+				status, err := utils.OctyneGetServerStatus(s)
+				if err != nil {
+					log.Fatalf("Error getting status for server %s: %v\n", s, err)
+				} else if status == utils.OctyneServerStatusStopped ||
+					status == utils.OctyneServerStatusCrashed {
+					break
+				}
+			}
+		}(server)
+	}
+	select {
+	case <-time.After(60 * time.Second):
+		log.Fatalln("Failed to stop all servers after 60 seconds! Exiting...")
+	case <-utils.WaitGroupDoneAsChannel(&wg):
+	}
+
 	interactivelyApplyUpdates(configs, softwareUpdates, serverPropertiesUpdates, pluginUpdates)
+
+	// Send start signals to all servers
+	fmt.Println("Starting affected servers via Octyne...")
+	for server := range affectedServers {
+		err := utils.OctyneStartServer(server)
+		if err != nil {
+			log.Printf("Error starting server %s: %v\n", server, err)
+		}
+	}
 }
--- utils/helpers.go
+++ utils/helpers.go
@@ -1,8 +1,19 @@
 package utils
 
+import "sync"
+
 func PickNonEmptyString(a, b string) string {
 	if a != "" {
 		return a
 	}
 	return b
+}
+
+func WaitGroupDoneAsChannel(wg *sync.WaitGroup) <-chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+	return ch
 }
